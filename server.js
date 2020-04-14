eee3c1a5888d382b39a0e9037c1a4cfc5eaf1059
JerielZi <jeriel_zinga@hotmail.com>
2020-04-14 23:42:36 +0100

    Installing package nodemon

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-const express = require('express');
+const express = require('express')
+const server = express()
+
+server.get("/", function(req, res) {
+  return res.send("Hi How are you?")
+})
+
+server.listen(5000, function(){
+  console.log("Server is running")
+})
